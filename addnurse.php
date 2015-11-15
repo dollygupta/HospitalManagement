f5ccccd7c7b0a22d285be0137a4bf1add46c1055
madhavvasudev <madhavvasudev@gmail.com>
2015-11-15 00:27:48 -0800

    Finished Add Nurse and List All Nurse

---
+++ addnurse.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+    <meta charset="UTF-8">
+    <title>Add a Nurse</title>
+</head>
+
+<body>
+<?php
+ini_set('display_errors', 1);
+
+$name = filter_input(INPUT_POST, "name");
+$email = filter_input(INPUT_POST, "email");
+$address = filter_input(INPUT_POST, "address");
+$salary = filter_input(INPUT_POST, "salary");
+$id = NULL;
+$emptype = "Nurse";
+$password = filter_input(INPUT_POST, "email");
+
+try
+{
+    if (empty($name) || empty($email) || empty($address) || empty($salary)) {
+        throw new Exception("Missing Details.");
+    }
+
+    // Connect to the database.
+    $con = new PDO("mysql:host=localhost;dbname=hos_m","root", "");
+    $con->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+    $query = "INSERT INTO employee (Name, Email, Address, Salary, EID, type, Password)
+              VALUES (:name, :email, :address, :salary, :id, :emptype, :password)";
+
+    $ps = $con->prepare($query);
+    $ps->execute(array(':name' => $name, ':email' => $email, ':address' => $address, ':salary' => $salary, ':id' => $id, ':emptype' => $emptype, ':password' => $password));
+
+   // echo $name, $email, $address, $salary, $id, $emptype, $password;
+    print "<h3>Successfully Inserted</h3>\n";
+}
+
+catch(PDOException $ex) {
+    echo 'ERROR: '.$ex->getMessage();
+}
+
+catch(Exception $ex) {
+    echo 'ERROR: '.$ex->getMessage();
+}
+/**
+ * Created by PhpStorm.
+ * User: madhav
+ * Date: 11/14/15
+ * Time: 6:51 PM
+ */
+?>
+</body>
+</html>
