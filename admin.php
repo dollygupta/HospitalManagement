d3a4c1d9f28abe98a79157925c8d8cc2d3c68604
Karthik Venkataraman <Karthik Venkataraman>
2015-11-17 13:16:59 -0800

    changes to doctor.php

--- admin.php
+++ admin.php
@@ -1,6 +1,6 @@
-<?include('adminCommon.html');?>
+<?php include('adminCommon.html');?>
 <div class="content">
     <h2>Admin Dashboard</h2>
 </div>
-<?include('footer.html');?>
+<?php include('footer.html');?>
     
--- doctor.php
+++ doctor.php
@@ -1,5 +1,11 @@
-<html>
-<body>
-	<h1>Doctor Dashboard</h1>
-</body>
-</html>
+<?php include('adminCommon.html');?>
+<div class="content">
+    <h2>Doctor Dashboard</h2>
+    <?php
+    echo "HI";
+$name=filter_input(INPUT_GET, "email");
+echo $name;
+    ?>
+</div>
+<?php include('footer.html');?>
+    
