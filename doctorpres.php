6350cb7af9035898ce23c77fa88debcf00c95e43
Karthik Venkataraman <Karthik Venkataraman>
2015-11-24 12:53:40 -0800

    doctorprescription updated

--- doctorpres.php
+++ doctorpres.php
@@ -1,5 +1,25 @@
 <?php
 include('doctorcommon.html');
-
-
 ?>
+<legend>Medicine Details</legend>
+<form  action="" method="post">
+<p>
+Pid:
+<input type="text" name = "name" />
+</p>
+<p>
+Medicine Name:
+<input type="text" name = "name" />
+</p>
+<p>
+Medicine Type:
+<input type="text" name = "type" />
+</p>
+<p>
+Date:
+<input type="text" name="date" />
+</p>
+<p>
+<INPUT TYPE = "Submit" Name = "Submit1" VALUE = "Save">
+</p>
+</form>
